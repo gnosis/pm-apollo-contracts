b4b29133881540aa808d94b318ff0d0be9c2d2c0
Alan Lu <alanlu1023@gmail.com>
2017-10-23 16:10:46 -0500

    Deployed to Rinkeby and bump to 0.1.1

--- truffle.js
+++ truffle.js
@@ -1,9 +1,31 @@
-module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+const config = {
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*",
+        },
+        rinkeby: {
+            host: "localhost",
+            port: 8545,
+            network_id: "4",
+        },
+    },
+}
+
+const merge = require('lodash.merge')
+
+try {
+    merge(config, require('./truffle-local'))
+}
+catch(e) {
+    if(e.code === 'MODULE_NOT_FOUND') {
+        // eslint-disable-next-line no-console
+        console.log('No local truffle config found. Using all defaults...')
+    } else {
+        // eslint-disable-next-line no-console
+        console.warn('Tried processing local config but got error:', e)
     }
-  }
-};
+}
+
+module.exports = config
